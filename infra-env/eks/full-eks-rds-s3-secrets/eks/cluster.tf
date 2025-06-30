46c67bb23a5e277aee67b466ff023f97bdfe0157
Ooi Yee Fei <yeefeiooi@gmail.com>
2025-06-30 20:47:51 +0800

    add AmazonEBSCSIDriverPolicy to node group iam role

--- infra-env/eks/full-eks-rds-s3-secrets/eks/cluster.tf
+++ infra-env/eks/full-eks-rds-s3-secrets/eks/cluster.tf
@@ -32,6 +32,10 @@ module "eks" {
       instance_types = ["t3.medium", "t3.large"]
       capacity_type  = "ON_DEMAND"
 
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+
       # Enable detailed monitoring
       enable_monitoring = true
 
