401fef46a5de7e2bb8e8435e1da71f511baba092
Ooi Yee Fei <yeefeiooi@gmail.com>
2025-07-07 13:52:37 +0800

    add subnet tags for karpenter discovery

--- infra-env/ai-on-eks/jark-stack/aws-networking/main.tf
+++ infra-env/ai-on-eks/jark-stack/aws-networking/main.tf
@@ -26,6 +26,7 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"         = "1"
+    "karpenter.sh/discovery"                  = var.cluster_name
   }
 
   tags = local.tags
